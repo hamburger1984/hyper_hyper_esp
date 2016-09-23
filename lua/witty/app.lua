960fcf6d26a92c3a7c7d10b475defb8cbac247a9
Andreas Krohn <hamburger1984@gmail.com>
2016-09-23 12:44:06 +0200

    hsv to rgb -- almost there..

--- lua/witty/app.lua
+++ lua/witty/app.lua
@@ -1,40 +1,64 @@
 local module = {}
 
-MAX = 0x400
-
 -- Color -- IO Index -- PIN
 --   RED           8     15
 -- GREEN           6     12
 --  BLUE           7     13
 PIN_R, PIN_G, PIN_B= 8, 6, 7
 
-r, g, b, l = 0, MAX, 0, MAX/2
+h, s, v = 24, 0xff, 0x10
 
-pulse, step = 0, 10
-min_pulse, max_pulse = 0, MAX/2
+pulse, step = 100, 9
 
-local function scale(component)
-    return (pulse * component * l) / (MAX*MAX)
+-- where:
+--   hue         in 0..255
+--   saturation  in 0..255
+--   value       in 0..255
+local function hsv_to_rgb(hue, saturation, value)
+    -- 0..5
+    local region = hue/43
+    -- 0..255
+    local fpart = (hue - (region*43))*6;
+
+    local p = bit.rshift(value * (255 - saturation), 8)
+    local q = bit.rshift(value * (255 - bit.rshift(saturation * fpart, 8)), 8)
+    local t = bit.rshift(value * (255 - bit.rshift(saturation * (255-fpart), 8)), 8)
+
+    if h==0 or h==6 then
+        return value, t, p
+    elseif h==1 then
+        return q, value, p
+    elseif h==2 then
+        return p, value, t
+    elseif h==3 then
+        return p, q, value
+    elseif h==4 then
+        return t, p, value
+    else
+        return value, p, q
+    end
 end
 
 local function setcolor()
-    pwm.setduty(PIN_R, scale(r))
-    pwm.setduty(PIN_G, scale(g))
-    pwm.setduty(PIN_B, scale(b))
+    local r, g, b = hsv_to_rgb(h, s, (v*pulse)/0x400)
+    pwm.setduty(PIN_R, r*4)
+    pwm.setduty(PIN_G, g*4)
+    pwm.setduty(PIN_B, b*4)
 end
 
 local function randomcolor()
-    r, g, b = math.random(MAX), math.random(MAX), math.random(MAX)
+    h = math.random(0xff)
+
     setcolor()
 end
 
 local function do_step()
-    if (pulse + step) > max_pulse or (pulse + step) < min_pulse then
+    if (pulse + step) > 0xff or (pulse + step) < 0 then
         step = -1 * step
     end
 
     pulse = pulse + step
-    l = adc.read(0)
+    v = adc.read(0)
 
     setcolor()
 end
@@ -47,13 +71,15 @@ function module.start()
         return
     end
 
-    pwm.setup(PIN_R, 300, scale(r))
-    pwm.setup(PIN_G, 300, scale(g))
-    pwm.setup(PIN_B, 300, scale(b))
+    pwm.setup(PIN_R, 300, 0)
+    pwm.setup(PIN_G, 300, 0)
+    pwm.setup(PIN_B, 300, 0)
 
     pwm.start(PIN_R)
     pwm.start(PIN_G)
     pwm.start(PIN_B)
+
+    randomcolor()
 
     tmr.alarm(0, 2000, tmr.ALARM_AUTO, function() randomcolor() end)
     tmr.alarm(1, 50, tmr.ALARM_AUTO, function() do_step() end)
