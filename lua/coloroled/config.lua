6c6aeac54708a9ea1105b755968308992c0e12ad
Andreas Krohn <hamburger1984@gmail.com>
2016-10-12 14:11:22 +0200

    Updated esptool submodule. Changed coloroled mqtt topic.

--- lua/coloroled/config.lua
+++ lua/coloroled/config.lua
@@ -8,6 +8,6 @@ module.mqtt_client_id = string.format("nodemcu-%06x", node.chipid())
 module.mqtt_timeout = 30
 module.mqtt_broker = "broker.hivemq.com"
 module.mqtt_port = 1883
-module.mqtt_topic = "nodemcu/#"
+module.mqtt_topic = "nodemcu/+/brightness"
 
 return module
